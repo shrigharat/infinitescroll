b1987dfa641d1d4dab032f8ab47a2cb90652a211
Shrishail Gharat <gharatshri014@gmail.com>
2021-07-14 18:00:40 +0530

    Fixed error and reached end footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useRef, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import "./App.css";
 import useQuery from "./useQuery.hook";
 import Post from "./components/post.component";
@@ -6,7 +6,23 @@ import Spinner from "./components/spinner.component";
 
 function App() {
   const [pageNo, setPageNo] = useState(1);
+  const storedDarkMode = localStorage.getItem("DARK_MODE");
+  const [darkMode, setDarkMode] = useState(storedDarkMode=="true");
   const { loading, error, posts, limitReached } = useQuery(pageNo);
+
+  const toggleDarkMode = () => {
+    setDarkMode(prev => !prev);
+  };
+
+  useEffect(() => {
+    console.log("Is in dark mode:", darkMode);
+    console.log(typeof darkMode);
+    localStorage.setItem("DARK_MODE", darkMode);
+
+    return () => {
+      console.log("Is in dark mode:", darkMode);
+    }
+  }, [darkMode]);
 
   const observer = useRef();
   const lastElementRef = useCallback(
@@ -30,17 +46,22 @@ function App() {
   );
 
   return (
-    <div className="App">
+    <div className="App" data-theme={darkMode ? "dark" : "light"}>
+      <div className="header">
+        <button className="dm-toggle" onClick={toggleDarkMode}>
+          {darkMode ? <i className="fas fa-sun"></i> : <i className="fas fa-moon"></i>}
+        </button>
+      </div>
       {posts.map((post, index) => {
         if (index + 1 === posts.length) {
-          return <Post post={post} ref={lastElementRef} key={post.id}/>
+          return <Post post={post} ref={lastElementRef} key={post.id} />;
         } else {
-          return <Post post={post} key={post.id}/>
+          return <Post post={post} key={post.id} />;
         }
       })}
-      {loading && <Spinner/>}
-      {error && <span>"Error occured..."</span>}
-      {limitReached && <strong>"You have reached the end!"</strong>}
+      {loading && <Spinner />}
+      {error && <span>An error occured 😞</span>}
+      {limitReached && <span className="end-reached">You have reached the end 🙂</span>}
     </div>
   );
 }
